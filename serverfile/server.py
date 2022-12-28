19650a9ac2cac8712ae446fae6a21bdb9a90e616
chijay69 <chijay69@outlook.com>
2022-12-28 09:45:17 +0100

    committed

--- serverfile/server.py
+++ serverfile/server.py
@@ -6,26 +6,43 @@ import threading
 import socketserver
 from configparser import ConfigParser
 
+# initialize logging
 logging.basicConfig(level=logging.DEBUG, format='%(name)s: %(message)s')
 
 # initialise config object
 config = ConfigParser()
 
+# create empty set
 my_set = set()
 
+
 def search_text(file_path, search_key):
+	'''A function that seaches through a file for a search_key'''
+	# initialize global variable
 	global my_set
+	# checks if the search_key is empty or filled with spaces
 	if not search_key.endswith(' ') and (search_key !=''):
+		# use a try except block to catch exceptions and handle accordingly
 		try:
+			# open a file 
 			with open(file_path) as f:
+				# create a loop
 				while True:
+					# read a line and add the line to a set
 					my_set.add(f.readline().rstrip())
+					# checks if no more lines to read
 					if not f.readline():
+						# breaks the loop if the check worked
 						break
+			# checks if search_key is in the set
 			if search_key in my_set:
+				# returns true if it checks out
 				return True
+		# catches name error exception
 		except NameError:
+			# handle the exception
 			print ('File does not exist, please comfirm file path')
+	# return false if the search key could not be found
 	return False
 		
 
@@ -40,10 +57,12 @@ class ThreadedFileRequestHandler(socketserver.StreamRequestHandler):
 		return socketserver.StreamRequestHandler.setup(self)
 		
 	def handle(self):
+		'''A handle function to handle the logic in the socket server'''
 		self.logger.debug('handle')
 
-		# client
+		# opens a try except block
 		try:
+			# get the ip address and port of the connected client
 			ip, port = self.client_address
 		except :
 			print('Ensure ip and port address are provided')
@@ -53,6 +72,7 @@ class ThreadedFileRequestHandler(socketserver.StreamRequestHandler):
 		
 		# accepts string from client connecction
 		while True:
+			# opens a try except block
 			try:
 				# read config file
 				config.read("/home/unix/AlgoSci/config.ini")
@@ -60,32 +80,42 @@ class ThreadedFileRequestHandler(socketserver.StreamRequestHandler):
 				# get config values
 				REREAD_ON_QUERY = algosci.getboolean("reread_on_query")
 				filepath = algosci.get("linuxpath")
-
 			except (KeyError, ValueError, NameError):
 				print("Cannot Parse configuration, check config file")
 
+			# read the string entered by the client
 			string = self.rfile.readline().decode('utf-8')
+
 			self.logger.debug('recv() => "%s"', string)
 
+			# checks if a string was sent
 			if not string:
+				# if no string was found break the loop
 				break
 
-			# get the execution time
+			# strip the sring of newline character
 			string = string.strip('\n')
+			# get the execution time
 			start = time.monotonic_ns()
 
+			# check if flag is set to True
 			if REREAD_ON_QUERY == True:
+				# if true read from file, call search_text function
 				conditional = search_text(filepath, string)
-			if REREAD_ON_QUERY == False :
+			else:
+				# if false read from set
 				conditional = string in my_set
-				
+
 			end = time.monotonic_ns()
+
+			# checks if conditional is True or False
 			if conditional:
 				self.logger.debug('execution time: {}ms'.format((end - start)//1000000))
 				self.wfile.write(b"STRING EXISTS\n")
 			else:
 				self.logger.debug('execution time: {}ms'.format((end - start)//1000000))
 				self.wfile.write(b"STRING NOT FOUND\n")
+
 		self.logger.debug(f"{self.client_address} closed connection")
 		
 	def finish(self):
@@ -104,17 +134,21 @@ class ThreadedFileServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 	logging =  True
 	
 	
-def start_server(address):		
+def start_server(address):
+	'''Function that starts the server '''
+	# opens a try except block
 	try:
+		# create server instance 
 		with ThreadedFileServer(address, ThreadedFileRequestHandler) as server:
 			print("Server is running...")
+			# keep the server running 
 			server.serve_forever()
-	except Exception as e:
-		print(f'An Exception {e} Occured')
+	except ConnectionError:
+		print('Could not start server')
 
 
 if __name__ =='__main__':
-	host = "127.0.0.1"
-	port = 49995
-	address = (host, port)
+	# set ip address and port number to be bound
+	address = ("127.0.0.1", 49995)
+	# call start server function to start the server
 	start_server(address)
